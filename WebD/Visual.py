ee8234ae0d718f2fbc94bb21e075a30a78f358e0
maifeeulasad <maifeeulasad@gmail.com>
2019-01-31 13:49:52 +0600

    visual upgrade

--- WebD/Visual.py
+++ WebD/Visual.py
@@ -4,12 +4,13 @@ import matplotlib.pyplot as plt
 G = nx.MultiDiGraph()
 
 G.add_edges_from([
-    (1, 2),
+    (0, 2),
     (2, 3),
     (3, 2),
     (2, 1),
+    (0, 1),
 ])
 
-plt.figure(figsize=(2,2))
-nx.draw(G)
+plt.figure(figsize=(5,5))
+nx.draw(G,with_labels=True)
 plt.show()
