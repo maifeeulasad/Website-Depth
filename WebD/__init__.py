feb77dd2230f5e132c00057cab299772862d5945
maifeeulasad <maifeeulasad@gmail.com>
2019-01-31 15:31:33 +0600

    update

--- WebD/__init__.py
+++ WebD/__init__.py
@@ -13,20 +13,11 @@ class Data:
                 res.insert(0,(x,y))
         return res
 
-    @staticmethod
-    def hello():
-        print("hello")
-
 
     @staticmethod
     def visualize():
-
         G = nx.MultiDiGraph()
-
         G.add_edges_from(Data.get_links_as_list())
-
         plt.figure(figsize=(5, 5))
         nx.draw(G, with_labels=True)
         plt.show()
-
-
